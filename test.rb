4bc89193c6820032392f9c3efe7d90c7344aa017
Dan Hwang <danhwangfkpc@gmail.com>
2019-05-20 21:20:34 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,12 +3,12 @@ def say_hi
 end 
 
 def get_name 
-  prints "What's your name?"
+  print "What's your name?"
   gets.chomp 
 end 
 
 def say_welcome(name)
-  prints "Welcome #{name}"
+  print "Welcome #{name}"
 end
 
 say_hi 
